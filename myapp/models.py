6caf21d2f890c385f33414b224b9d01876f97937
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2025-04-05 02:14:04 +0100

    adding more detailed page for questions

--- myapp/models.py
+++ myapp/models.py
@@ -9,3 +9,11 @@ class Task(models.Model):
 
 	def __str__(self):
 		return self.title
+
+class Question(models.Model):
+	title = models.CharField(max_length=200)
+	body = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self .title
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 # Create your views here.
 def home(request):
 	return render(request, 'home.html')
+
+def question_list(request):
+	questions = Question.objects.all()
+	return render(request, 'question_list.html', {'questions': questions})
+
+def question_detail(request, id):
+	question = get_object_or_404(Question, id=id)
+	return render(request, 'question_detail.html', {'question': question})
